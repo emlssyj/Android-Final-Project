5d2f4cfd74998de167221da8c9c0c9377d8d6093
emlssyj <793413451@qq.com>
2020-01-04 16:43:32 +0800

    Final Edition

---
+++ app/src/main/java/com/letter/days/anniversary/AnniPagerTransformer.kt
@@ -0,0 +1,30 @@
+package com.letter.days.anniversary
+
+import android.support.v4.view.ViewPager
+import android.view.View
+import kotlin.math.abs
+
+
+class AnniPagerTransformer: ViewPager.PageTransformer {
+
+    private var viewPager: ViewPager? = null
+
+    override fun transformPage(view: View, position: Float) {
+
+        if (viewPager == null) {
+            viewPager = view.parent as ViewPager
+        }
+
+        val leftInScreen = view.left - viewPager?.scrollX!!
+        val centerXInViewPager = leftInScreen + view.measuredWidth / 2
+        val offsetX = centerXInViewPager - viewPager?.measuredWidth!! / 2
+        val offsetRate = offsetX * 0.38f / viewPager?.measuredWidth!!
+        val scaleFactor = 1 - abs(offsetRate)
+
+        if (scaleFactor > 0) {
+            view.scaleX = scaleFactor
+            view.scaleY = scaleFactor
+        }
+
+    }
+}
---
+++ app/src/main/java/com/letter/days/widget/AdjustTextView.kt
@@ -0,0 +1,27 @@
+package com.letter.days.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.TextView
+
+
+class AdjustTextView : TextView {
+
+    constructor(context: Context, attrs: AttributeSet?=null, defStyleAttr: Int, defStyleRes: Int):
+            super(context, attrs, defStyleAttr, defStyleRes)
+
+    constructor(context: Context, attrs: AttributeSet?=null, defStyleAttr: Int):
+            super(context, attrs, defStyleAttr)
+
+    constructor(context: Context, attrs: AttributeSet?=null):
+            super(context, attrs)
+
+    constructor(context: Context):
+            super(context)
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val width = MeasureSpec.getSize(widthMeasureSpec)
+        val height = MeasureSpec.makeMeasureSpec(width, MeasureSpec.EXACTLY)
+        super.onMeasure(widthMeasureSpec, height)
+    }
+}
